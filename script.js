8cac2927766b00265ba2868b468c983407a648ad
mas-alissa <89016425+mas-alissa@users.noreply.github.com>
2021-09-25 12:41:20 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("hallo Winc Academy studente");
+
+// Dit is een grote som (this is regel comment .. so i can see the rest of the code - i can see the result in terminal)
+
+/* i can not see this in console becouse they are in blok comment
+console.log(1230941 * 1823794);
+
+console.log(637263 / 54);
+*/
